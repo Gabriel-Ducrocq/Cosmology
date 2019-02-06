a908a9dc8adb06d8ffb68bc27c3208eab7b27b2c
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-02-06 18:13:29 +0100

    3k sample size

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import time
 NSIDE = 2
 sigma_rbf = 100000
 N_PROCESS_MAX = 45
-N_sample = 1000
+N_sample = 3000
 
 COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_s", "tau_reio"]
 COSMO_PARAMS_MEANS = [0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561]
